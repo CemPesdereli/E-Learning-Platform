ea9238e6ac61ca72483802871e5b9c71552af111
CemPesdereli <cempesdereli@gmail.com>
2024-07-03 20:05:40 +0300

    init project

---
+++ src/test/java/com/cem/elearningplatform/ElearningplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cem.elearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
