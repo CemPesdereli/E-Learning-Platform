8e1449e697ece686d09b3068ace6daad573be36d
CemPesdereli <cempesdereli@gmail.com>
2024-07-06 07:51:06 +0300

    ddl changed, author entity added

---
+++ src/main/java/com/cem/elearningplatform/models/Author.java
@@ -0,0 +1,54 @@
+package com.cem.elearningplatform.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue()
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private int age;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
